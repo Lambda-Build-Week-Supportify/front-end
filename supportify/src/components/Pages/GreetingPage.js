269db77ff55a2ea4b7143ab37060185e5ef65539
CJ Lucido <c.jose.lucido@gmail.com>
2019-11-18 14:16:32 -0800

    added grid with 2 boxes for login boxes with shadows

--- supportify/src/components/Pages/GreetingPage.js
+++ supportify/src/components/Pages/GreetingPage.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
 ////////////ORGANISMS
 import Login from '../Organisms/Login'
 import SignUp from '../Organisms/SignUp'
 
+
+///////////////MATERIAL UI///////////////
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { Card } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+     paper: {
+        color: "blue",
+        
+     },
+    
+    control: {
+      padding: theme.spacing(2),
+    
+    },
+  card: {
+    minWidth: 275,
+    color: "red",
+    marginLeft: "25%"
+  },
+  card2:{
+    minWidth: 275,
+    color: "red",
+    marginLeft: "15%",
+    // boxShadow: "3"doesnt work
+  }
+}
+  ));
+
 export default function GreetingPage(){
+  
+      const classes = useStyles();
 
 
     return (
@@ -15,17 +53,29 @@ export default function GreetingPage(){
             <header>
                     Header
             </header>
-            <div>
-                Ghana Admin
-                <Login/>
-            </div>
-            <div>
-                US Boardmember
-                <Login/>
-            </div>
-            <div>
+            <Grid container className={classes.root}  spacing={1}>
+                    <Box className={classes.card} boxShadow={3}>
+                            <Paper className={classes.paper}>
+                                
+                                    Ghana Admin
+                                    <Login/>
+                               
+                            </Paper>
+                    </Box>  
+                       
+                    <Box className={classes.card2} >   
+                            <Paper className={classes.paper} boxShadow={3}>
+                                
+                                    US Boardmember
+                                    <Login/>
+                               
+                            </Paper>
+                     </Box> 
+                   
+              
+            </Grid>
                 <SignUp/>
-            </div>
+            
         </div>
     )
 }
