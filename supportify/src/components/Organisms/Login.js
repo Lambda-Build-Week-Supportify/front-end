52afaecfe462554a00da8af64e0e1205131f59f7
CJ Lucido <c.jose.lucido@gmail.com>
2019-11-18 17:37:34 -0800

    login will now be conditional-> depends on whether admin true or not

--- supportify/src/components/Organisms/Login.js
+++ supportify/src/components/Organisms/Login.js
@@ -16,20 +16,16 @@ const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
-  //  const [userInput000, setUserInput] = useState({'username': "", "password": ""})
 
-  // const handleChange = type => e => {
-  //    setUserInput({ ...userInput000, type: userInput000[type] = e.target.value })
-  // }
   const handleUserChange = () => e => {
     e.preventDefault()
-   props.status == false ? props.updateUserBoard(e.target.value) : props.updateUserName(e.target.value) 
+   props.status === false ? props.updateUserBoard(e.target.value) : props.updateUserName(e.target.value) 
  }
  
 
  const handlePassChange = () => e => {
   e.preventDefault()
- props.status == false ? props.updatePassBoard(e.target.value) : props.updatePassWord(e.target.value) 
+ props.status === false ? props.updatePassBoard(e.target.value) : props.updatePassWord(e.target.value) 
 }
 
   const login = (payload) => {
@@ -49,10 +45,19 @@ const Login = (props) => {
 
   const handleLogin = (e) => {
       e.preventDefault()
-      let captureEntries = {
-          username: props.userInput['username'],
-          password: props.userInput['password']
+      let captureEntries ={}
+      if(props.admin === false){
+        captureEntries = {
+          username: props.userInputBoard['username'],
+          password: props.userInputBoard['password']
       }
+      }else{
+          captureEntries = {
+            username: props.userInput['username'],
+            password: props.userInput['password']
+        }
+      }
+     
       login(captureEntries)
   
  
@@ -65,8 +70,8 @@ const Login = (props) => {
     props.adminToTrue()
   }
 }
-  const usernameStateValue = props.greetName == "boardname" ? props.userInputBoard['username'] : props.userInput['username']
-  const passwordStateValue = props.greetName == "boardname" ? props.userInputBoard['password'] : props.userInput['password']
+  const usernameStateValue = props.greetName === "boardname" ? props.userInputBoard['username'] : props.userInput['username']
+  const passwordStateValue = props.greetName === "boardname" ? props.userInputBoard['password'] : props.userInput['password']
  // props.userInput['username']
 // because it has to check if props.admin equals true it breaks the simultaneous updating of each login form. Still, the values should be different for whether this is true or not so I will make another userInput-like value to accept Board member login credentials. This will require changing the null in the falsey to the new userInput-like value
 
@@ -83,8 +88,8 @@ console.log("this is props.userInput",props.userInput)
 
 //solved separation with props.greetName
 
-const inputName = props.admin == false ? "boardname": "username"
-const inputPass = props.admin == false ? "passboard": "password"
+const inputName = props.admin === false ? "boardname": "username"
+const inputPass = props.admin === false ? "passboard": "password"
 
   return (
     <>
